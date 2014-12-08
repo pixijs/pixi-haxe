4f78ff8532dd36d7a538d80bd6902d01b0fdf454
Adi Mora <adi.mora@gamesys.co.uk>
2014-12-08 13:11:06 +0000

    #10 updated AbstractFilter class

--- pixi/filters/AbstractFilter.hx
+++ pixi/filters/AbstractFilter.hx
@@ -8,10 +8,10 @@ extern class AbstractFilter {
 	 * If you want to make a custom filter this should be your base class.
 	 * @class AbstractFilter
 	 * @constructor
-	 * @param fragmentSrc
-	 * @param uniforms
+	 * @param [fragmentSrc] {Array<String>}
+	 * @param uniforms {Dynamic}
 	 */
-	function new(fragmentSrc:Array<String>, uniforms:Dynamic);
+	function new(?fragmentSrc:Array<String>, ?uniforms:Dynamic);
 
 	/**
 	 * Syncs the uniforms between the class object and the shaders.
@@ -22,13 +22,41 @@ extern class AbstractFilter {
 
 	/**
 	* @property dirty
-	* @type Boolean
+	* @type Bool
 	*/
 	var dirty:Bool;
 
 	/**
 	* @property padding
-	* @type Number
+	* @type Float
 	*/
 	var padding:Float;
+
+	/**
+	* An array of passes - some filters contain a few steps this array simply stores the steps in a liniear fashion.
+	* For example the blur filter has two passes blurX and blurY.
+	* @property passes
+	* @type Array<AbstractFilter> an array of filter objects
+	*/
+	var passes:Array<AbstractFilter>;
+
+	/**
+	* @property shaders
+	* @type Array an array of shaders
+	*/
+	var shaders:Array<Dynamic>;
+
+	/**
+	* @property uniforms
+	* @type object
+	* @private
+	*/
+	var uniforms:Dynamic;
+
+	/**
+	* @property fragmentSrc
+	* @type Array
+	* @private
+	*/
+	var fragmentSrc:Array<String>;
 }
