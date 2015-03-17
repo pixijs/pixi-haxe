478b0db5995a0779d5624befde29d299458a9c65
Adi Mora <adi.mora@gamesys.co.uk>
2015-03-17 13:57:13 +0000

    like for like bunnymark3

---
+++ samples/_output/bunnymark3.js
@@ -0,0 +1,192 @@
+(function () { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var Std = function() { };
+Std.random = function(x) {
+	if(x <= 0) return 0; else return Math.floor(Math.random() * x);
+};
+var pixi = {};
+pixi.plugins = {};
+pixi.plugins.app = {};
+pixi.plugins.app.Application = function() {
+	this._lastTime = new Date();
+	this._setDefaultValues();
+};
+pixi.plugins.app.Application.prototype = {
+	_setDefaultValues: function() {
+		this.pixelRatio = 1;
+		this.skipFrame = false;
+		this.backgroundColor = 0;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._skipFrame = false;
+	}
+	,start: function(renderer,stats) {
+		if(stats == null) stats = true;
+		if(renderer == null) renderer = "auto";
+		var _this = window.document;
+		this._canvas = _this.createElement("canvas");
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		this._canvas.style.position = "absolute";
+		window.document.body.appendChild(this._canvas);
+		this._stage = new PIXI.Container();
+		var renderingOptions = { };
+		renderingOptions.view = this._canvas;
+		renderingOptions.backgroundColor = this.backgroundColor;
+		renderingOptions.resolution = this.pixelRatio;
+		if(renderer == "auto") this._renderer = PIXI.autoDetectRenderer(this.width,this.height,renderingOptions); else if(renderer == "recommended") this._renderer = PIXI.autoDetectRecommendedRenderer(this.width,this.height,renderingOptions); else if(renderer == "canvas") this._renderer = new PIXI.CanvasRenderer(this.width,this.height,renderingOptions); else this._renderer = new PIXI.WebGLRenderer(this.width,this.height,renderingOptions);
+		window.document.body.appendChild(this._renderer.view);
+		window.onresize = $bind(this,this._onWindowResize);
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		this._lastTime = new Date();
+		if(stats) this._addStats();
+	}
+	,_onWindowResize: function(event) {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._renderer.resize(this.width,this.height);
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		if(this._stats != null) {
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+		}
+		if(this.onResize != null) this.onResize();
+	}
+	,_onRequestAnimationFrame: function() {
+		if(this.skipFrame && this._skipFrame) this._skipFrame = false; else {
+			this._skipFrame = true;
+			this._calculateElapsedTime();
+			if(this.onUpdate != null) this.onUpdate(this._elapsedTime);
+			this._renderer.render(this._stage);
+		}
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		if(this._stats != null) this._stats.update();
+	}
+	,_calculateElapsedTime: function() {
+		this._currentTime = new Date();
+		this._elapsedTime = this._currentTime.getTime() - this._lastTime.getTime();
+		this._lastTime = this._currentTime;
+	}
+	,_addStats: function() {
+		if(window.Stats != null) {
+			var _container = window.document.createElement("div");
+			window.document.body.appendChild(_container);
+			this._stats = new Stats();
+			this._stats.domElement.style.position = "absolute";
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+			_container.appendChild(this._stats.domElement);
+			this._stats.begin();
+		}
+	}
+};
+var samples = {};
+samples.bunnymark3 = {};
+samples.bunnymark3.Bunny = function(texture) {
+	PIXI.Sprite.call(this,texture);
+};
+samples.bunnymark3.Bunny.__super__ = PIXI.Sprite;
+samples.bunnymark3.Bunny.prototype = $extend(PIXI.Sprite.prototype,{
+});
+samples.bunnymark3.Main = function() {
+	pixi.plugins.app.Application.call(this);
+	this._init();
+	this._sprites = [];
+	this._add = false;
+	this._minX = this._minY = 0;
+	this._maxX = window.innerWidth;
+	this._maxY = window.innerHeight;
+	this._bunnyTexture = PIXI.Texture.fromImage("assets/basics/bunny.png");
+	this._quantityLabel = new PIXI.Text("Press/Touch and hold to add bunnies continuously",{ font : "15px Tahoma", fill : "#FFFFFF"});
+	this._stage.addChild(this._quantityLabel);
+	this._renderer.view.onmousedown = $bind(this,this.onTouchStart);
+	this._renderer.view.onmouseup = $bind(this,this.onTouchEnd);
+	this._container = new PIXI.Container();
+	this._stage.addChild(this._container);
+};
+samples.bunnymark3.Main.main = function() {
+	new samples.bunnymark3.Main();
+};
+samples.bunnymark3.Main.__super__ = pixi.plugins.app.Application;
+samples.bunnymark3.Main.prototype = $extend(pixi.plugins.app.Application.prototype,{
+	_init: function() {
+		this.backgroundColor = 13158;
+		this.onUpdate = $bind(this,this._onUpdate);
+		pixi.plugins.app.Application.prototype.start.call(this);
+	}
+	,onTouchStart: function(event) {
+		this._add = true;
+	}
+	,onTouchEnd: function(event) {
+		this._add = false;
+	}
+	,_onUpdate: function(elapsedTime) {
+		if(this._add) this._addBunnys();
+		var _g1 = 0;
+		var _g = this._sprites.length;
+		while(_g1 < _g) {
+			var i = _g1++;
+			var bunny = this._sprites[i];
+			bunny.position.x += bunny.speedX;
+			bunny.position.y += bunny.speedY;
+			bunny.speedY += 2;
+			if(bunny.position.x > this._maxX) {
+				bunny.speedX *= -1;
+				bunny.position.x = this._maxX;
+			} else if(bunny.position.x < this._minX) {
+				bunny.speedX *= -1;
+				bunny.position.x = this._minX;
+			}
+			if(bunny.position.y > this._maxY) {
+				bunny.speedY *= -0.9;
+				bunny.position.y = this._maxY;
+				if(Math.random() > 0.5) bunny.speedY -= Math.random() * 6;
+			} else if(bunny.position.y < this._minY) {
+				bunny.speedY = 0;
+				bunny.position.y = this._minY;
+			}
+		}
+	}
+	,_addBunnys: function() {
+		var _g = 0;
+		while(_g < 10) {
+			var i = _g++;
+			this._bunny = new samples.bunnymark3.Bunny(this._bunnyTexture);
+			this._bunny.name = "bunny" + i;
+			this._container.addChild(this._bunny);
+			this._bunny.x = Std.random(this._maxX);
+			this._bunny.y = Std.random(this._maxY);
+			this._bunny.anchor.set(0.5,0.5);
+			this._bunny.speedX = Math.random() * 16 + 2;
+			this._bunny.speedY = Math.random() * 16 - 10;
+			this._bunny.rotationSpeed = Math.random() / 50 + 0.01;
+			this._bunny.scaleSpeed = Math.random() / 50 + 0.01;
+			this._sprites.push(this._bunny);
+		}
+		this._updateQuanityLabel();
+	}
+	,_updateQuanityLabel: function() {
+		this._quantityLabel.text = "Quantity: " + this._sprites.length;
+	}
+});
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+Math.NaN = Number.NaN;
+Math.NEGATIVE_INFINITY = Number.NEGATIVE_INFINITY;
+Math.POSITIVE_INFINITY = Number.POSITIVE_INFINITY;
+Math.isFinite = function(i) {
+	return isFinite(i);
+};
+Math.isNaN = function(i1) {
+	return isNaN(i1);
+};
+samples.bunnymark3.Main.main();
+})();
+
+//# sourceMappingURL=bunnymark3.js.map
---
+++ samples/bunnymark3/Bunny.hx
@@ -0,0 +1,16 @@
+package samples.bunnymark3;
+
+import pixi.core.textures.Texture;
+import pixi.core.sprites.Sprite;
+
+class Bunny extends Sprite {
+
+	public var speedX(default, default):Float;
+	public var speedY(default, default):Float;
+	public var scaleSpeed(default, default):Float;
+	public var rotationSpeed(default, default):Float;
+
+	public function new(texture:Texture) {
+		super(texture);
+	}
+}
