ccfe5fcc6436f864d307db1551e8cbd6f5207664
Adi Mora <adi.mora@gamesys.co.uk>
2015-03-24 23:02:49 +0000

    loader

---
+++ pixi/loaders/Loader.hx
@@ -0,0 +1,27 @@
+package pixi.loaders;
+
+import pixi.plugins.resourceloader.ResourceLoader;
+
+@:native("PIXI.loaders.Loader")
+extern class Loader extends ResourceLoader {
+
+	/**
+	 *
+	 * The new loader, extends Resource Loader by Chad Engler : https://github.com/englercj/resource-loader
+	 *
+	 * ```js
+	 * var loader = new PIXI.loader();
+	 *
+	 * loader.add('spineboy',"data/spineboy.json");
+	 *
+	 * loader.once('complete',onAssetsLoaded);
+	 *
+	 * loader.load();
+	 * ```
+	 *
+	 * @class
+	 * @extends ResourceLoader
+	 * @memberof PIXI.loaders
+	 */
+	function new();
+}
