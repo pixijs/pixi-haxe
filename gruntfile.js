15e45dc8fc296d0520274e90e85585b6c9294858
Adi Mora <adi.mora@gamesys.co.uk>
2015-03-28 09:09:03 +0000

    generating pixi js files from node

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,59 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
+         pkg: grunt.file.readJSON("package.json"),
 
-        haxe: {
-            project: {
-                hxml: "build.hxml -xml docs.xml --macro include('pixi')"
-            }
-        },
+         haxe: {
+             project: {
+                 hxml: "build.hxml -xml docs.xml --macro include('pixi')"
+             }
+         },
 
-        exec: {
-            docs: "haxelib run chxdoc -o ../adireddy.github.io/docs/haxe-pixi/v3 -f docs.xml " +
-                " --showTodoTags=false " +
-                " --deny=demos.*,samples.*,js.*,haxe.*,nape.*,zpp_nape.*,/ " +
-                "--title='Externs of pixi.js for Haxe' " +
-                "--subtitle='<a href='http://adireddy.github.io/haxe-pixi' target='_blank'>haxe-pixi</a>'",
-            copy: "cp -R samples/_output/** ../adireddy.github.io/demos/haxe-pixi/v3/",
-            copy: "cp -R ./libs/** ./samples/_output/libs/"
-        },
+         browserify: {
+             pixi: {
+                 src: [],
+                 dest: "libs/pixi.dev.js",
+                 options: {
+                     require: ["pixi.js"],
+                     browserifyOptions: {
+                         standalone: "PIXI"
+                     }
+                 }
+             }
+         },
 
-        zip: {
-            "pixi.zip": ["pixi/**", "haxelib.json"]
-        }
-    });
+         uglify: {
+             options: {
+                 compress: {
+                     drop_console: true
+                 }
+             },
+             target: {
+                 files: {
+                     "libs/pixi.min.js": ["libs/pixi.dev.js"]
+                 }
+             }
+         },
+
+         exec: {
+             copy_libs: "cp -R ./libs/*.min.js ./samples/_output/libs/",
+             docs: "haxelib run chxdoc -o ../adireddy.github.io/docs/haxe-pixi/v3 -f docs.xml " +
+                   " --showTodoTags=false " +
+                   " --deny=demos.*,samples.*,js.*,haxe.*,nape.*,zpp_nape.*,/ " +
+                   "--title='Externs of pixi.js for Haxe' " +
+                   "--subtitle='<a href='http://adireddy.github.io/haxe-pixi' target='_blank'>haxe-pixi</a>'",
+             copy_samples: "cp -R samples/_output/** ../adireddy.github.io/demos/haxe-pixi/v3/"
+         },
+
+         zip: {
+             "pixi.zip": ["pixi/**", "haxelib.json"]
+         }
+     });
 
     grunt.loadNpmTasks("grunt-haxe");
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks("grunt-zip");
     grunt.loadNpmTasks("grunt-exec");
-    grunt.registerTask("default", ["haxe", "exec", "zip"]);
+    grunt.registerTask("default", ["haxe", "browserify", "uglify", "exec"]);
 };
