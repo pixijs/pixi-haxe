5aa25371974d20596814b2eac1d0a05a167c594f
Adi Mora <adi.mora@gamesys.co.uk>
2015-04-11 10:28:12 +0100

    added tiling demo

---
+++ samples/_output/tiling.js
@@ -0,0 +1,130 @@
+(function () { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var pixi = {};
+pixi.plugins = {};
+pixi.plugins.app = {};
+pixi.plugins.app.Application = function() {
+	this._lastTime = new Date();
+	this._setDefaultValues();
+};
+pixi.plugins.app.Application.prototype = {
+	_setDefaultValues: function() {
+		this.pixelRatio = 1;
+		this.skipFrame = false;
+		this.autoResize = true;
+		this.backgroundColor = 16777215;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._skipFrame = false;
+	}
+	,start: function(renderer,stats,parentDom) {
+		if(stats == null) stats = true;
+		if(renderer == null) renderer = "auto";
+		var _this = window.document;
+		this._canvas = _this.createElement("canvas");
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		this._canvas.style.position = "absolute";
+		if(parentDom == null) window.document.body.appendChild(this._canvas); else parentDom.appendChild(this._canvas);
+		this._stage = new PIXI.Container();
+		var renderingOptions = { };
+		renderingOptions.view = this._canvas;
+		renderingOptions.backgroundColor = this.backgroundColor;
+		renderingOptions.resolution = this.pixelRatio;
+		renderingOptions.antialias = this.antialias;
+		renderingOptions.autoResize = this.autoResize;
+		if(renderer == "auto") this._renderer = PIXI.autoDetectRenderer(this.width,this.height,renderingOptions); else if(renderer == "canvas") this._renderer = new PIXI.CanvasRenderer(this.width,this.height,renderingOptions); else this._renderer = new PIXI.WebGLRenderer(this.width,this.height,renderingOptions);
+		window.document.body.appendChild(this._renderer.view);
+		if(this.autoResize) window.onresize = $bind(this,this._onWindowResize);
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		this._lastTime = new Date();
+		if(stats) this._addStats();
+	}
+	,_onWindowResize: function(event) {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._renderer.resize(this.width,this.height);
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		if(this._stats != null) {
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+		}
+		if(this.onResize != null) this.onResize();
+	}
+	,_onRequestAnimationFrame: function() {
+		if(this.skipFrame && this._skipFrame) this._skipFrame = false; else {
+			this._skipFrame = true;
+			this._calculateElapsedTime();
+			if(this.onUpdate != null) this.onUpdate(this._elapsedTime);
+			this._renderer.render(this._stage);
+		}
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		if(this._stats != null) this._stats.update();
+	}
+	,_calculateElapsedTime: function() {
+		this._currentTime = new Date();
+		this._elapsedTime = this._currentTime.getTime() - this._lastTime.getTime();
+		this._lastTime = this._currentTime;
+	}
+	,_addStats: function() {
+		if(window.Stats != null) {
+			var _container = window.document.createElement("div");
+			window.document.body.appendChild(_container);
+			this._stats = new Stats();
+			this._stats.domElement.style.position = "absolute";
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+			_container.appendChild(this._stats.domElement);
+			this._stats.begin();
+		}
+	}
+};
+var samples = {};
+samples.tiling = {};
+samples.tiling.Main = function() {
+	pixi.plugins.app.Application.call(this);
+	this._init();
+};
+samples.tiling.Main.main = function() {
+	new samples.tiling.Main();
+};
+samples.tiling.Main.__super__ = pixi.plugins.app.Application;
+samples.tiling.Main.prototype = $extend(pixi.plugins.app.Application.prototype,{
+	_init: function() {
+		this.backgroundColor = 9946478;
+		this.onUpdate = $bind(this,this._onUpdate);
+		pixi.plugins.app.Application.prototype.start.call(this);
+		var texture = PIXI.Texture.fromImage("assets/tiling/p2.jpeg");
+		this._tilingSprite = new PIXI.extras.TilingSprite(texture,this.width,this.height);
+		this._stage.addChild(this._tilingSprite);
+		this._count = 0;
+	}
+	,_onUpdate: function(elapsedTime) {
+		this._count += 0.005;
+		this._tilingSprite.tileScale.x = 2 + Math.sin(this._count);
+		this._tilingSprite.tileScale.y = 2 + Math.cos(this._count);
+		this._tilingSprite.tilePosition.x += 1;
+		this._tilingSprite.tilePosition.y += 1;
+	}
+});
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+Math.NaN = Number.NaN;
+Math.NEGATIVE_INFINITY = Number.NEGATIVE_INFINITY;
+Math.POSITIVE_INFINITY = Number.POSITIVE_INFINITY;
+Math.isFinite = function(i) {
+	return isFinite(i);
+};
+Math.isNaN = function(i1) {
+	return isNaN(i1);
+};
+samples.tiling.Main.main();
+})();
+
+//# sourceMappingURL=tiling.js.map
---
+++ samples/tiling/Main.hx
@@ -0,0 +1,42 @@
+package samples.tiling;
+
+import pixi.core.textures.Texture;
+import pixi.extras.TilingSprite;
+import pixi.plugins.app.Application;
+
+class Main extends Application {
+
+	var _tilingSprite:TilingSprite;
+	var _count:Float;
+
+	public function new() {
+		super();
+		_init();
+	}
+
+	function _init() {
+		backgroundColor = 0x97C56E;
+		onUpdate = _onUpdate;
+		super.start();
+
+		var texture = Texture.fromImage("assets/tiling/p2.jpeg");
+		_tilingSprite = new TilingSprite(texture, width, height);
+
+		_stage.addChild(_tilingSprite);
+
+		_count = 0;
+	}
+
+	function _onUpdate(elapsedTime:Float) {
+		_count += 0.005;
+		_tilingSprite.tileScale.x = 2 + Math.sin(_count);
+		_tilingSprite.tileScale.y = 2 + Math.cos(_count);
+
+		_tilingSprite.tilePosition.x += 1;
+		_tilingSprite.tilePosition.y += 1;
+	}
+
+	static function main() {
+		new Main();
+	}
+}
