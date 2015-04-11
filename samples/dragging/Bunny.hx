ce0a37ebb37a9748a0b931dfccce6b21da217674
Adi Mora <adi.mora@gamesys.co.uk>
2015-04-11 10:30:45 +0100

    dragging demo

---
+++ samples/dragging/Bunny.hx
@@ -0,0 +1,15 @@
+package samples.dragging;
+
+import pixi.interaction.InteractionData;
+import pixi.core.textures.Texture;
+import pixi.core.sprites.Sprite;
+
+class Bunny extends Sprite {
+
+	public var data:InteractionData;
+	public var dragging:Bool;
+
+	public function new(texture:Texture) {
+		super(texture);
+	}
+}
