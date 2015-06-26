85f7df41bb526e2bf4c09d1909971e842619dbff
Adi Mora <adi.mora@gamesys.co.uk>
2015-06-26 09:55:54 +0100

    device info sample

---
+++ samples/_output/deviceinfo.js
@@ -0,0 +1,137 @@
+(function (console) { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var pixi_plugins_app_Application = function() {
+	this._lastTime = new Date();
+	this._setDefaultValues();
+};
+pixi_plugins_app_Application.prototype = {
+	set_fps: function(val) {
+		this._frameCount = 0;
+		return val >= 1 && val < 60?this.fps = val | 0:this.fps = 60;
+	}
+	,set_skipFrame: function(val) {
+		if(val) {
+			console.log("pixi.plugins.app.Application > Deprecated: skipFrame - use fps property and set it to 30 instead");
+			this.set_fps(30);
+		}
+		return this.skipFrame = val;
+	}
+	,_setDefaultValues: function() {
+		this.pixelRatio = 1;
+		this.set_skipFrame(false);
+		this.autoResize = true;
+		this.transparent = false;
+		this.antialias = false;
+		this.forceFXAA = false;
+		this.backgroundColor = 16777215;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.set_fps(60);
+	}
+	,start: function(renderer,stats,parentDom) {
+		if(stats == null) stats = true;
+		if(renderer == null) renderer = "auto";
+		var _this = window.document;
+		this._canvas = _this.createElement("canvas");
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		this._canvas.style.position = "absolute";
+		if(parentDom == null) window.document.body.appendChild(this._canvas); else parentDom.appendChild(this._canvas);
+		this._stage = new PIXI.Container();
+		var renderingOptions = { };
+		renderingOptions.view = this._canvas;
+		renderingOptions.backgroundColor = this.backgroundColor;
+		renderingOptions.resolution = this.pixelRatio;
+		renderingOptions.antialias = this.antialias;
+		renderingOptions.forceFXAA = this.forceFXAA;
+		renderingOptions.autoResize = this.autoResize;
+		renderingOptions.transparent = this.transparent;
+		if(renderer == "auto") this._renderer = PIXI.autoDetectRenderer(this.width,this.height,renderingOptions); else if(renderer == "canvas") this._renderer = new PIXI.CanvasRenderer(this.width,this.height,renderingOptions); else this._renderer = new PIXI.WebGLRenderer(this.width,this.height,renderingOptions);
+		window.document.body.appendChild(this._renderer.view);
+		if(this.autoResize) window.onresize = $bind(this,this._onWindowResize);
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		this._lastTime = new Date();
+		if(stats) this._addStats();
+	}
+	,_onWindowResize: function(event) {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._renderer.resize(this.width,this.height);
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		if(this._stats != null) {
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+		}
+		if(this.onResize != null) this.onResize();
+	}
+	,_onRequestAnimationFrame: function() {
+		this._frameCount++;
+		if(this._frameCount == (60 / this.fps | 0)) {
+			this._frameCount = 0;
+			this._calculateElapsedTime();
+			if(this.onUpdate != null) this.onUpdate(this._elapsedTime);
+			this._renderer.render(this._stage);
+		}
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		if(this._stats != null) this._stats.update();
+	}
+	,_calculateElapsedTime: function() {
+		this._currentTime = new Date();
+		this._elapsedTime = this._currentTime.getTime() - this._lastTime.getTime();
+		this._lastTime = this._currentTime;
+	}
+	,_addStats: function() {
+		if(window.Stats != null) {
+			var _container = window.document.createElement("div");
+			window.document.body.appendChild(_container);
+			this._stats = new Stats();
+			this._stats.domElement.style.position = "absolute";
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+			_container.appendChild(this._stats.domElement);
+			this._stats.begin();
+		}
+	}
+};
+var samples_deviceinfo_Main = function() {
+	pixi_plugins_app_Application.call(this);
+	this._init();
+};
+samples_deviceinfo_Main.main = function() {
+	new samples_deviceinfo_Main();
+};
+samples_deviceinfo_Main.__super__ = pixi_plugins_app_Application;
+samples_deviceinfo_Main.prototype = $extend(pixi_plugins_app_Application.prototype,{
+	_init: function() {
+		this.backgroundColor = 13158;
+		pixi_plugins_app_Application.prototype.start.call(this);
+		this._rendererType = ["Unknown","WebGL","Canvas"];
+		this._info = new PIXI.Container();
+		this._stage.addChild(this._info);
+		var txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Resolution: \t\t" + window.innerWidth + " x " + window.innerHeight;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Pixel Ratio: \t\t" + window.devicePixelRatio;
+		txt.y = 40;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Renderer: \t\t\t\t\t" + this._rendererType[this._renderer.type];
+		txt.y = 80;
+		this._info.addChild(txt);
+		this._info.position.x = (window.innerWidth - this._info.width) / 2;
+		this._info.position.y = (window.innerHeight - this._info.height) / 2;
+	}
+});
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+samples_deviceinfo_Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
+
+//# sourceMappingURL=deviceinfo.js.map
---
+++ samples/deviceinfo/Main.hx
@@ -0,0 +1,49 @@
+package samples.deviceinfo;
+
+import pixi.core.display.Container;
+import pixi.core.text.Text;
+import pixi.plugins.app.Application;
+import js.Browser;
+
+class Main extends Application {
+
+	var _info:Container;
+
+	var _rendererType:Array<String>;
+
+	public function new() {
+		super();
+		_init();
+	}
+
+	function _init() {
+		backgroundColor = 0x003366;
+		super.start();
+
+		_rendererType = ["Unknown", "WebGL", "Canvas"];
+
+		_info = new Container();
+		_stage.addChild(_info);
+
+		var txt = new Text("", {fill: "#FFFFFF"});
+		txt.text = "Resolution: \t\t" + Browser.window.innerWidth + " x " + Browser.window.innerHeight;
+		_info.addChild(txt);
+
+		txt = new Text("", {fill: "#FFFFFF"});
+		txt.text = "Pixel Ratio: \t\t" + Browser.window.devicePixelRatio;
+		txt.y = 40;
+		_info.addChild(txt);
+
+		txt = new Text("", {fill: "#FFFFFF"});
+		txt.text = "Renderer: \t\t\t\t\t" + _rendererType[_renderer.type];
+		txt.y = 80;
+		_info.addChild(txt);
+
+		_info.position.x = (Browser.window.innerWidth - _info.width) / 2;
+		_info.position.y = (Browser.window.innerHeight - _info.height) / 2;
+	}
+
+	static function main() {
+		new Main();
+	}
+}
