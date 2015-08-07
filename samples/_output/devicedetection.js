e713dc0b80e580f1d03274b1c47d12be865616c4
Adi Mora <adi.mora@gamesys.co.uk>
2015-08-07 11:00:06 +0100

    sample

---
+++ samples/_output/devicedetection.js
@@ -0,0 +1,259 @@
+(function (console) { "use strict";
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var EReg = function(r,opt) {
+	opt = opt.split("u").join("");
+	this.r = new RegExp(r,opt);
+};
+EReg.__name__ = true;
+EReg.prototype = {
+	match: function(s) {
+		if(this.r.global) this.r.lastIndex = 0;
+		this.r.m = this.r.exec(s);
+		this.r.s = s;
+		return this.r.m != null;
+	}
+};
+Math.__name__ = true;
+var Std = function() { };
+Std.__name__ = true;
+Std.string = function(s) {
+	return js_Boot.__string_rec(s,"");
+};
+var js_Boot = function() { };
+js_Boot.__name__ = true;
+js_Boot.__string_rec = function(o,s) {
+	if(o == null) return "null";
+	if(s.length >= 5) return "<...>";
+	var t = typeof(o);
+	if(t == "function" && (o.__name__ || o.__ename__)) t = "object";
+	switch(t) {
+	case "object":
+		if(o instanceof Array) {
+			if(o.__enum__) {
+				if(o.length == 2) return o[0];
+				var str2 = o[0] + "(";
+				s += "\t";
+				var _g1 = 2;
+				var _g = o.length;
+				while(_g1 < _g) {
+					var i1 = _g1++;
+					if(i1 != 2) str2 += "," + js_Boot.__string_rec(o[i1],s); else str2 += js_Boot.__string_rec(o[i1],s);
+				}
+				return str2 + ")";
+			}
+			var l = o.length;
+			var i;
+			var str1 = "[";
+			s += "\t";
+			var _g2 = 0;
+			while(_g2 < l) {
+				var i2 = _g2++;
+				str1 += (i2 > 0?",":"") + js_Boot.__string_rec(o[i2],s);
+			}
+			str1 += "]";
+			return str1;
+		}
+		var tostr;
+		try {
+			tostr = o.toString;
+		} catch( e ) {
+			return "???";
+		}
+		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
+			var s2 = o.toString();
+			if(s2 != "[object Object]") return s2;
+		}
+		var k = null;
+		var str = "{\n";
+		s += "\t";
+		var hasp = o.hasOwnProperty != null;
+		for( var k in o ) {
+		if(hasp && !o.hasOwnProperty(k)) {
+			continue;
+		}
+		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__" || k == "__properties__") {
+			continue;
+		}
+		if(str.length != 2) str += ", \n";
+		str += s + k + " : " + js_Boot.__string_rec(o[k],s);
+		}
+		s = s.substring(1);
+		str += "\n" + s + "}";
+		return str;
+	case "function":
+		return "<function>";
+	case "string":
+		return o;
+	default:
+		return String(o);
+	}
+};
+var pixi_plugins_app_Application = function() {
+	this._lastTime = new Date();
+	this._setDefaultValues();
+};
+pixi_plugins_app_Application.__name__ = true;
+pixi_plugins_app_Application.prototype = {
+	set_fps: function(val) {
+		this._frameCount = 0;
+		return val >= 1 && val < 60?this.fps = val | 0:this.fps = 60;
+	}
+	,set_skipFrame: function(val) {
+		if(val) {
+			console.log("pixi.plugins.app.Application > Deprecated: skipFrame - use fps property and set it to 30 instead");
+			this.set_fps(30);
+		}
+		return this.skipFrame = val;
+	}
+	,_setDefaultValues: function() {
+		this.pixelRatio = 1;
+		this.set_skipFrame(false);
+		this.autoResize = true;
+		this.transparent = false;
+		this.antialias = false;
+		this.forceFXAA = false;
+		this.backgroundColor = 16777215;
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.set_fps(60);
+	}
+	,start: function(renderer,stats,parentDom) {
+		if(stats == null) stats = true;
+		if(renderer == null) renderer = "auto";
+		var _this = window.document;
+		this._canvas = _this.createElement("canvas");
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		this._canvas.style.position = "absolute";
+		if(parentDom == null) window.document.body.appendChild(this._canvas); else parentDom.appendChild(this._canvas);
+		this._stage = new PIXI.Container();
+		var renderingOptions = { };
+		renderingOptions.view = this._canvas;
+		renderingOptions.backgroundColor = this.backgroundColor;
+		renderingOptions.resolution = this.pixelRatio;
+		renderingOptions.antialias = this.antialias;
+		renderingOptions.forceFXAA = this.forceFXAA;
+		renderingOptions.autoResize = this.autoResize;
+		renderingOptions.transparent = this.transparent;
+		if(renderer == "auto") this._renderer = PIXI.autoDetectRenderer(this.width,this.height,renderingOptions); else if(renderer == "canvas") this._renderer = new PIXI.CanvasRenderer(this.width,this.height,renderingOptions); else this._renderer = new PIXI.WebGLRenderer(this.width,this.height,renderingOptions);
+		window.document.body.appendChild(this._renderer.view);
+		if(this.autoResize) window.onresize = $bind(this,this._onWindowResize);
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		this._lastTime = new Date();
+		if(stats) this._addStats();
+	}
+	,_onWindowResize: function(event) {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this._renderer.resize(this.width,this.height);
+		this._canvas.style.width = this.width + "px";
+		this._canvas.style.height = this.height + "px";
+		if(this._stats != null) {
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+		}
+		if(this.onResize != null) this.onResize();
+	}
+	,_onRequestAnimationFrame: function() {
+		this._frameCount++;
+		if(this._frameCount == (60 / this.fps | 0)) {
+			this._frameCount = 0;
+			this._calculateElapsedTime();
+			if(this.onUpdate != null) this.onUpdate(this._elapsedTime);
+			this._renderer.render(this._stage);
+		}
+		window.requestAnimationFrame($bind(this,this._onRequestAnimationFrame));
+		if(this._stats != null) this._stats.update();
+	}
+	,_calculateElapsedTime: function() {
+		this._currentTime = new Date();
+		this._elapsedTime = this._currentTime.getTime() - this._lastTime.getTime();
+		this._lastTime = this._currentTime;
+	}
+	,_addStats: function() {
+		if(window.Stats != null) {
+			var _container = window.document.createElement("div");
+			window.document.body.appendChild(_container);
+			this._stats = new Stats();
+			this._stats.domElement.style.position = "absolute";
+			this._stats.domElement.style.top = "2px";
+			this._stats.domElement.style.right = "2px";
+			_container.appendChild(this._stats.domElement);
+			this._stats.begin();
+		}
+	}
+};
+var samples_devicedetection_Main = function() {
+	pixi_plugins_app_Application.call(this);
+	this._init();
+};
+samples_devicedetection_Main.__name__ = true;
+samples_devicedetection_Main.main = function() {
+	new samples_devicedetection_Main();
+};
+samples_devicedetection_Main.__super__ = pixi_plugins_app_Application;
+samples_devicedetection_Main.prototype = $extend(pixi_plugins_app_Application.prototype,{
+	_init: function() {
+		this.backgroundColor = 13158;
+		pixi_plugins_app_Application.prototype.start.call(this);
+		this._info = new PIXI.Container();
+		this._stage.addChild(this._info);
+		var txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Android:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" + Std.string(this._isAndroid());
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Android Phone:\t\t\t\t\t" + Std.string(this._isAndroidPhone());
+		txt.y = 40;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "iOS:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" + Std.string(this._isiOS());
+		txt.y = 80;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "iOS Phone:\t\t\t\t\t\t\t\t\t\t\t\t\t" + Std.string(this._isiOSPhone());
+		txt.y = 120;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Windows:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" + Std.string(this._isWindows());
+		txt.y = 160;
+		this._info.addChild(txt);
+		txt = new PIXI.Text("",{ fill : "#FFFFFF"});
+		txt.text = "Windows Phone:\t\t\t" + Std.string(this._isWindowsPhone());
+		txt.y = 200;
+		this._info.addChild(txt);
+		this._info.position.x = (window.innerWidth - this._info.width) / 2;
+		this._info.position.y = (window.innerHeight - this._info.height) / 2;
+	}
+	,_isAndroid: function() {
+		return new EReg("Android","i").match(window.navigator.userAgent);
+	}
+	,_isAndroidPhone: function() {
+		return new EReg("Android","i").match(window.navigator.userAgent) && new EReg("Mobile","i").match(window.navigator.userAgent);
+	}
+	,_isiOS: function() {
+		return new EReg("(iPad|iPhone|iPod)","i").match(window.navigator.userAgent);
+	}
+	,_isiOSPhone: function() {
+		return new EReg("(iPhone|iPod)","i").match(window.navigator.userAgent);
+	}
+	,_isWindows: function() {
+		return new EReg("(Windows|iemobile)","i").match(window.navigator.userAgent);
+	}
+	,_isWindowsPhone: function() {
+		return new EReg("Windows Phone","i").match(window.navigator.userAgent) || new EReg("iemobile","i").match(window.navigator.userAgent);
+	}
+});
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+String.__name__ = true;
+Array.__name__ = true;
+Date.__name__ = ["Date"];
+samples_devicedetection_Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
+
+//# sourceMappingURL=devicedetection.js.map
