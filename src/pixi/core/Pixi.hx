d0df8125c7fb2cc254fc5ead9c0c6c0c956575cc
Adi <adiinteractive@gmail.com>
2016-04-04 13:42:09 +0100

    added new constants

--- src/pixi/core/Pixi.hx
+++ src/pixi/core/Pixi.hx
@@ -8,26 +8,26 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {string} VERSION
+	 * @property {String} VERSION
 	 */
 	static var VERSION:String;
 
 	/**
-     * @property {number} PI_2 - Two Pi
+     * @property {Float} PI_2 - Two Pi
      * @constant
      * @static
      */
 	static var PI_2:Float;
 
 	/**
-     * @property {number} RAD_TO_DEG - Constant conversion factor for converting radians to degrees
+     * @property {Float} RAD_TO_DEG - Constant conversion factor for converting radians to degrees
      * @constant
      * @static
      */
 	static var RAD_TO_DEG:Float;
 
 	/**
-     * @property {Number} DEG_TO_RAD - Constant conversion factor for converting degrees to radians
+     * @property {Float} DEG_TO_RAD - Constant conversion factor for converting degrees to radians
      * @constant
      * @static
      */
@@ -38,7 +38,7 @@ extern class Pixi {
      *
      * @static
      * @constant
-     * @property {number} TARGET_FPMS=0.06
+     * @property {Float} TARGET_FPMS=0.06
      */
 	static var TARGET_FPMS:Float;
 
@@ -47,10 +47,10 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {object} RENDERER_TYPE
-	 * @property {number} RENDERER_TYPE.UNKNOWN
-	 * @property {number} RENDERER_TYPE.WEBGL
-	 * @property {number} RENDERER_TYPE.CANVAS
+	 * @property {RendererType} RENDERER_TYPE
+	 * @property {Int} RENDERER_TYPE.UNKNOWN
+	 * @property {Int} RENDERER_TYPE.WEBGL
+	 * @property {Int} RENDERER_TYPE.CANVAS
 	 */
 	static var RENDERER_TYPE:RendererType;
 
@@ -61,26 +61,43 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {object} BLEND_MODES
-	 * @property {number} BLEND_MODES.NORMAL
-	 * @property {number} BLEND_MODES.ADD
-	 * @property {number} BLEND_MODES.MULTIPLY
-	 * @property {number} BLEND_MODES.SCREEN
-	 * @property {number} BLEND_MODES.OVERLAY
-	 * @property {number} BLEND_MODES.DARKEN
-	 * @property {number} BLEND_MODES.LIGHTEN
-	 * @property {number} BLEND_MODES.COLOR_DODGE
-	 * @property {number} BLEND_MODES.COLOR_BURN
-	 * @property {number} BLEND_MODES.HARD_LIGHT
-	 * @property {number} BLEND_MODES.SOFT_LIGHT
-	 * @property {number} BLEND_MODES.DIFFERENCE
-	 * @property {number} BLEND_MODES.EXCLUSION
-	 * @property {number} BLEND_MODES.HUE
-	 * @property {number} BLEND_MODES.SATURATION
-	 * @property {number} BLEND_MODES.COLOR
-	 * @property {number} BLEND_MODES.LUMINOSITY
+	 * @property {BlendModes} BLEND_MODES
+	 * @property {Int} BLEND_MODES.NORMAL
+	 * @property {Int} BLEND_MODES.ADD
+	 * @property {Int} BLEND_MODES.MULTIPLY
+	 * @property {Int} BLEND_MODES.SCREEN
+	 * @property {Int} BLEND_MODES.OVERLAY
+	 * @property {Int} BLEND_MODES.DARKEN
+	 * @property {Int} BLEND_MODES.LIGHTEN
+	 * @property {Int} BLEND_MODES.COLOR_DODGE
+	 * @property {Int} BLEND_MODES.COLOR_BURN
+	 * @property {Int} BLEND_MODES.HARD_LIGHT
+	 * @property {Int} BLEND_MODES.SOFT_LIGHT
+	 * @property {Int} BLEND_MODES.DIFFERENCE
+	 * @property {Int} BLEND_MODES.EXCLUSION
+	 * @property {Int} BLEND_MODES.HUE
+	 * @property {Int} BLEND_MODES.SATURATION
+	 * @property {Int} BLEND_MODES.COLOR
+	 * @property {Int} BLEND_MODES.LUMINOSITY
 	 */
 	static var BLEND_MODES:BlendModes;
+
+	/**
+     * Various webgl draw modes. These can be used to specify which GL drawMode to use
+     * under certain situations and renderers.
+     *
+     * @static
+     * @constant
+     * @property {DrawModes} DRAW_MODES
+     * @property {Int} DRAW_MODES.POINTS
+     * @property {Int} DRAW_MODES.LINES
+     * @property {Int} DRAW_MODES.LINE_LOOP
+     * @property {Int} DRAW_MODES.LINE_STRIP
+     * @property {Int} DRAW_MODES.TRIANGLES
+     * @property {Int} DRAW_MODES.TRIANGLE_STRIP
+     * @property {Int} DRAW_MODES.TRIANGLE_FAN
+     */
+	static var DRAW_MODES:DrawModes;
 
 	/**
 	 * The scale modes that are supported by pixi.
@@ -90,12 +107,55 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {object} SCALE_MODES
-	 * @property {number} SCALE_MODES.DEFAULT=LINEAR
-	 * @property {number} SCALE_MODES.LINEAR Smooth scaling
-	 * @property {number} SCALE_MODES.NEAREST Pixelating scaling
+	 * @property {ScaleModes} SCALE_MODES
+	 * @property {Int} SCALE_MODES.DEFAULT=LINEAR
+	 * @property {Int} SCALE_MODES.LINEAR Smooth scaling
+	 * @property {Int} SCALE_MODES.NEAREST Pixelating scaling
 	 */
 	static var SCALE_MODES:ScaleModes;
+
+	/**
+     * The wrap modes that are supported by pixi.
+     *
+     * The DEFAULT wrap mode affects the default wraping mode of future operations.
+     * It can be re-assigned to either CLAMP or REPEAT, depending upon suitability.
+     * If the texture is non power of two then clamp will be used regardless as webGL can only use REPEAT if the texture is po2
+     * This property only affects WebGL
+     * @static
+     * @constant
+     * @property {WrapModes} WRAP_MODES
+     * @property {Int} WRAP_MODES.DEFAULT=CLAMP
+     * @property {Int} WRAP_MODES.CLAMP The textures uvs are clamped
+     * @property {Int} WRAP_MODES.REPEAT The texture uvs tile and repeat
+     * @property {Int} WRAP_MODES.MIRRORED_REPEAT The texture uvs tile and repeat with mirroring
+     */
+	static var WRAP_MODES:WrapModes;
+
+	/**
+     * The gc modes that are supported by pixi.
+     *
+     * The DEFAULT Garbage Collection mode for pixi textures is MANUAL
+     * If set to DEFAULT, the renderer will occasianally check textures usage. If they are not used for a specified period of time they will be removed from the GPU.
+     * They will of corse be uploaded again when they are required. This is a silent behind the scenes process that should ensure that the GPU does not  get filled up.
+     * Handy for mobile devices!
+     * This property only affects WebGL
+     * @static
+     * @constant
+     * @property {GCModes} GC_MODES
+     * @property {Int} GC_MODES.DEFAULT=DEFAULT
+     * @property {Int} GC_MODES.AUTO Garbage collection will happen periodically automatically
+     * @property {Int} GC_MODES.MANUAL Garbage collection will need to be called manually
+     */
+	static var GC_MODES:GCModes;
+
+	/**
+     * If set to true WebGL will attempt make textures mimpaped by default
+     * Mipmapping will only succeed if the base texture uploaded has power of two dimensions
+     * @static
+     * @constant
+     * @property {Bool} MIPMAP_TEXTURES
+     */
+	static var MIPMAP_TEXTURES:Bool;
 
 	/**
 	 * The prefix that denotes a URL is for a retina asset
@@ -117,16 +177,16 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {object} DEFAULT_RENDER_OPTIONS
+	 * @property {DefaultRenderOptions} DEFAULT_RENDER_OPTIONS
 	 * @property {HTMLCanvasElement} DEFAULT_RENDER_OPTIONS.view=null
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.transparent=false
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.antialias=false
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.forceFXAA=false
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.preserveDrawingBuffer=false
-	 * @property {number} DEFAULT_RENDER_OPTIONS.resolution=1
-	 * @property {number} DEFAULT_RENDER_OPTIONS.backgroundColor=0x000000
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.clearBeforeRender=true
-	 * @property {boolean} DEFAULT_RENDER_OPTIONS.autoResize=false
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.transparent=false
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.antialias=false
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.forceFXAA=false
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.preserveDrawingBuffer=false
+	 * @property {Float} DEFAULT_RENDER_OPTIONS.resolution=1
+	 * @property {Int} DEFAULT_RENDER_OPTIONS.backgroundColor=0x000000
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.clearBeforeRender=true
+	 * @property {Bool} DEFAULT_RENDER_OPTIONS.autoResize=false
 	 */
 	static var DEFAULT_RENDER_OPTIONS:DefaultRenderOptions;
 
@@ -135,17 +195,20 @@ extern class Pixi {
 	 *
 	 * @static
 	 * @constant
-	 * @property {object} SHAPES
-	 * @property {object} SHAPES.POLY=0
-	 * @property {object} SHAPES.RECT=1
-	 * @property {object} SHAPES.CIRC=2
-	 * @property {object} SHAPES.ELIP=3
-	 * @property {object} SHAPES.RREC=4
+	 * @property {Shapes} SHAPES
+	 * @property {Int} SHAPES.POLY=0
+	 * @property {Int} SHAPES.RECT=1
+	 * @property {Int} SHAPES.CIRC=2
+	 * @property {Int} SHAPES.ELIP=3
+	 * @property {Int} SHAPES.RREC=4
 	 */
 	static var SHAPES:Shapes;
 
+	//4096 - nice balance between mobile and desktop machines
 	static var SPRITE_BATCH_SIZE:Int;
-	//2000 - nice balance between mobile and desktop machines
+
+	static var SPRITE_MAX_TEXTURES:Int;
+
 }
 
 typedef RendererType = {
@@ -172,6 +235,19 @@ typedef ScaleModes = {
 	var NEAREST:Int;
 }
 
+typedef WrapModes = {
+	var DEFAULT:Int;
+	var CLAMP:Int;
+	var REPEAT:Int;
+	var MIRRORED_REPEAT:Int;
+}
+
+typedef GCModes = {
+	var DEFAULT:Int;
+	var AUTO:Int;
+	var MANUAL:Int;
+}
+
 typedef BlendModes = {
 	var NORMAL:Int;
 	var ADD:Int;
@@ -190,6 +266,16 @@ typedef BlendModes = {
 	var SATURATION:Int;
 	var COLOR:Int;
 	var LUMINOSITY:Int;
+}
+
+typedef DrawModes = {
+	var POINTS:Int;
+	var LINES:Int;
+	var LINE_LOOP:Int;
+	var LINE_STRIP:Int;
+	var TRIANGLES:Int;
+	var TRIANGLE_STRIP:Int;
+	var TRIANGLE_FAN:Int;
 }
 
 typedef Shapes = {
