b5703c25926adfc01026f0d5dbd28d497d7e5536
Adi <adiinteractive@gmail.com>
2017-05-10 09:30:45 +0100

    cleanup

--- src/pixi/core/RenderOptions.hx
+++ src/pixi/core/RenderOptions.hx
@@ -5,17 +5,20 @@ import js.html.CanvasElement;
 typedef RenderOptions = {
 	@:optional var width:Float;
 	@:optional var height:Float;
-	@:optional var noWebGL:Bool;
-	@:optional var sharedTicker:Bool;
-	@:optional var view:CanvasElement;
-	@:optional var resolution:Float;
+	@:optional var transparent:Bool;
 	@:optional var antialias:Bool;
+	@:optional var preserveDrawingBuffer:Bool;
+	@:optional var resolution:Float;
+	@:optional var forceCanvas:Bool;
+	@:optional var legacy:Bool;
+	@:optional var sharedTicker:Bool;
+	@:optional var sharedLoader:Bool;
+
+	@:optional var noWebGL:Bool;
+	@:optional var view:CanvasElement;
 	@:optional var forceFXAA:Bool;
 	@:optional var autoResize:Bool;
-	@:optional var transparent:Bool;
 	@:optional var backgroundColor:Int;
 	@:optional var clearBeforeRender:Bool;
-	@:optional var preserveDrawingBuffer:Bool;
 	@:optional var roundPixels:Bool;
-	@:optional var legacy:Bool;
 }
