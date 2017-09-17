ff31592e049ea87923a750f35de43460e21d5859
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2017-09-17 13:01:32 +0300

    spine.Slot extern

--- src/pixi/plugins/spine/core/Slot.hx
+++ src/pixi/plugins/spine/core/Slot.hx
@@ -1,7 +1,32 @@
 package pixi.plugins.spine.core;
+import pixi.plugins.spine.core.attachments.Attachment;
 
 
-//TODO
+@:native("PIXI.spine.core.Slot")
 extern class Slot 
 {
+	var currentMesh: Dynamic;
+	var currentSprite: Dynamic;
+	var currentGraphics: Dynamic;
+	var clippingContainer: Dynamic;
+	var meshes: Dynamic;
+	var currentMeshName: String;
+	var sprites: Dynamic;
+	var currentSpriteName: String;
+	var blendMode: Float;
+	//var tempRegion: TextureRegion;
+	var tempAttachment: Attachment;
+	var data: SlotData;
+	var bone: Bone;
+	var color: Color;
+	var darkColor: Color;
+	var attachment: Attachment;
+	var attachmentTime: Float;	//?
+	var attachmentVertices: Array<Float>;
+	function new(data: SlotData, bone: Bone):Void;
+	function getAttachment(): Attachment;
+	function setAttachment(attachment: Attachment): Void;
+	function setAttachmentTime(time: Float): Void;
+	function getAttachmentTime(): Float;
+	function setToSetupPose(): Void;
 }
