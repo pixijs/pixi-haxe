c6c4b4162ea4d33a9fba19aaf23aa1e54098d757
Mark Knol <mark.knol@gmail.com>
2019-12-18 15:20:06 +0100

    Animation

--- src/pixi/plugins/spine/core/IkConstraint.hx
+++ src/pixi/plugins/spine/core/IkConstraint.hx
@@ -1,8 +1,19 @@
 package pixi.plugins.spine.core;
 
-//TODO
-extern class IkConstraint 
-{
-
-	
+extern class IkConstraint implements Updatable {
+	var data: IkConstraintData;
+	var bones: Array<Bone>;
+	var target: Bone;
+	var bendDirection: Float;
+	var compress: Bool;
+	var stretch: Bool;
+	var mix: Float;
+	var softness: Float;
+	var active: Bool;
+	function new(data: IkConstraintData, skeleton: Skeleton);
+	function isActive(): Bool;
+	function apply(): Void;
+	function update(): Void;
+	function apply1(bone: Bone, targetX: Float, targetY: Float, compress: Bool, stretch: Bool, uniform: Bool, alpha: Float): Void;
+	function apply2(parent: Bone, child: Bone, targetX: Float, targetY: Float, bendDir: Float, stretch: Bool, softness: Float, alpha: Float): Void;
 }
--- src/pixi/plugins/spine/core/PathConstraint.hx
+++ src/pixi/plugins/spine/core/PathConstraint.hx
@@ -1,8 +1,34 @@
 package pixi.plugins.spine.core;
+import pixi.plugins.spine.core.attachments.PathAttachment;
 
 //TODO
-extern class PathConstraint 
+extern class PathConstraint implements Updatable
 {
-
+	static var NONE: Float;
+	static var BEFORE: Float;
+	static var AFTER: Float;
+	static var epsilon: Float;
+	var data: PathConstraintData;
+	var bones: Array<Bone>;
+	var target: Slot;
+	var position: Float;
+	var spacing: Float;
+	var rotateMix: Float;
+	var translateMix: Float;
+	var spaces: Array<Float>;
+	var positions: Array<Float>;
+	var world: Array<Float>;
+	var curves: Array<Float>;
+	var lengths: Array<Float>;
+	var segments: Array<Float>;
+	var active: Bool;
+	function new(data: PathConstraintData, skeleton: Skeleton);
+	function isActive(): Bool;
+	function apply(): Void;
+	function update(): Void;
+	function computeWorldPositions(path: PathAttachment, spacesCount: Float, tangents: Bool, percentPosition: Bool, percentSpacing: Bool): Array<Float>;
+	function addBeforePosition(p: Float, temp: Array<Float>, i: Float, out: Array<Float>, o: Float): Void;
+	function addAfterPosition(p: Float, temp: Array<Float>, i: Float, out: Array<Float>, o: Float): Void;
+	function addCurvePosition(p: Float, x1: Float, y1: Float, cx1: Float, cy1: Float, cx2: Float, cy2: Float, x2: Float, y2: Float, out: Array<Float>, o: Float, tangents: Bool): Void;
 	
 }
