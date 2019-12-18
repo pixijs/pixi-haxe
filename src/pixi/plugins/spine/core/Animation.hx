60229c02b9ae598ce57727bb0adc257d3dd61207
Mark Knol <mark.knol@gmail.com>
2019-12-18 15:02:50 +0100

    Update Animation.hx

--- src/pixi/plugins/spine/core/Animation.hx
+++ src/pixi/plugins/spine/core/Animation.hx
@@ -1,8 +1,15 @@
 package pixi.plugins.spine.core;
 
-//TODO
-extern class Animation 
-{
-
+extern class Animation {
+	public var name: String;
+	public var timelines: Array<Timeline>;
+	public var timelineIds: Array<Bool>;
+	public var duration: Float;
 	
+	public function new(name: String, timelines: Array<Timeline>, duration: Float);
+	public function hasTimeline(id: Float): Bool;
+	public function apply(skeleton: Skeleton, lastTime: Float, time: Float, loop: Bool, events: Array<Event>, alpha: Float, blend: MixBlend, direction: MixDirection): Void;
+	
+    public static function binarySearch(values: Array<Float>, target: Float, ?step: Float): Float;
+    public static function linearSearch(values: Array<Float>, target: Float, step: Float): Float;
 }
