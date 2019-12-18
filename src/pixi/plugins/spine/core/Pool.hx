4d55b644fde4c2a1decf9123e29280f29bad0d73
Mark Knol <mark.knol@gmail.com>
2019-12-18 17:55:47 +0100

    Update Pool.hx

--- src/pixi/plugins/spine/core/Pool.hx
+++ src/pixi/plugins/spine/core/Pool.hx
@@ -2,8 +2,8 @@ package pixi.plugins.spine.core;
 
 extern class Pool<T> {
 	private var items:Array<T>;
-	private var instantiator:() -> T;
-	function new(instantiator: () -> T);
+	private var instantiator:Void -> T;
+	function new(instantiator: Void -> T);
 	function obtain():T;
 	function free(item: T): Void;
 	function freeAll(items: Array<T>): Void;
