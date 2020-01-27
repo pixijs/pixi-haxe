9f5baf94d76a393435d558107b20533a248a4b27
Mark Knol <mark.knol@gmail.com>
2020-01-27 14:41:20 +0100

    Update Bone.hx

--- src/pixi/plugins/spine/core/Bone.hx
+++ src/pixi/plugins/spine/core/Bone.hx
@@ -1,24 +1,51 @@
 package pixi.plugins.spine.core;
+
 import pixi.core.math.Matrix;
 import pixi.plugins.dragonbones.model.BoneData;
 
 @:native("PIXI.spine.core.Bone")
-extern class Bone 
-{
-
-	public var children:Array<Bone>;
+extern class Bone {
+	static var yDown: Bool;
 	
-	public var matrix:Matrix;
+	var matrix: Matrix;
+	var worldX(default, never): Float;
+	var worldY(default, never): Float;
+	var data: BoneData;
+	var skeleton: Skeleton;
+	var parent: Bone;
+	var children: Array<Bone>;
+	var x: Float;
+	var y: Float;
+	var rotation: Float;
+	var scaleX: Float;
+	var scaleY: Float;
+	var shearX: Float;
+	var shearY: Float;
+	var ax: Float;
+	var ay: Float;
+	var arotation: Float;
+	var ascaleX: Float;
+	var ascaleY: Float;
+	var ashearX: Float;
+	var ashearY: Float;
+	var appliedValid: Bool;
+	var sorted: Bool;
+	var active: Bool;
 	
-	public var x:Float;
-	
-	public var y:Float;
-	
-	public var worldX:Float;
-	
-	public var worldY:Float;
-	
-	public var rotation:Float;
-	
-	public var data:BoneData;
+	function new(data: BoneData, skeleton: Skeleton, parent: Bone);
+	function isActive(): Bool;
+	function update(): Void;
+	function updateWorldTransform(): Void;
+	function updateWorldTransformWith(x: Float, y: Float, rotation: Float, scaleX: Float, scaleY: Float, shearX: Float, shearY: Float): Void;
+	function setToSetupPose(): Void;
+	function getWorldRotationX(): Float;
+	function getWorldRotationY(): Float;
+	function getWorldScaleX(): Float;
+	function getWorldScaleY(): Float;
+	function updateAppliedTransform(): Void;
+	function worldToLocal(world: Vector2): Vector2;
+	function localToWorld(local: Vector2): Vector2;
+	function worldToLocalRotation(worldRotation: Float): Float;
+	function localToWorldRotation(localRotation: Float): Float;
+	function rotateWorld(degrees: Float): Void;
 }
